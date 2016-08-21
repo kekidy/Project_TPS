2ab92bcaa5090c6e28f5d668541f6c800179e300
unknown <>
2016-08-22 06:01:58 +0900

    Idle 애니메이션 및 Aim 애니메이션 처리 추가

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+    [Header("IK Target")]
+    [SerializeField] private Transform leftHandTarget;
+
+	private Transform m_myTransform;
+    private Animator  m_myAnim;
+
+    private Transform m_cameraTransform;
+
+	void Awake () {
+        m_myTransform = transform;
+        m_myAnim      = GetComponent<Animator>();
+        m_cameraTransform = Camera.main.transform;
+	}
+	
+	void LateUpdate () {
+        m_myTransform.eulerAngles = new Vector3(m_myTransform.eulerAngles.x, m_cameraTransform.eulerAngles.y, m_myTransform.eulerAngles.z);
+	}
+
+    void OnAnimatorIK(int layer)
+    {
+        if (layer == 0)
+        {
+            m_myAnim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0.5f);
+            m_myAnim.SetIKPosition(AvatarIKGoal.LeftHand, leftHandTarget.position);
+        }
+    }
+}
---
+++ Assets/Scripts/TPSCameraCtrl.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TPSCameraCtrl : MonoBehaviour {
+    [SerializeField] private Transform m_ownerTransform;
+    [SerializeField] private Vector3   m_distanceOffset = Vector3.zero;
+    [SerializeField] private float     m_limitUpVerticalAngle   = 0f;
+    [SerializeField] private float     m_limitDownVerticalAngle = 0f;
+
+    private Animator  m_ownerAnim;
+
+    private Transform m_myTransform;
+
+    private float m_rotX;
+    private float m_rotY;
+
+	void Awake () {
+        m_myTransform    = transform;
+        m_ownerAnim      = m_ownerTransform.GetComponent<Animator>();
+	}
+
+	void Update () {
+        AngleCalculate();
+	}
+
+    void OnDrawGizmos()
+    {
+        if (m_ownerTransform != null && m_myTransform != null)
+        {
+            Gizmos.DrawLine(m_myTransform.position, m_ownerTransform.position + new Vector3(0f, m_distanceOffset.y, 0f));
+            Gizmos.DrawLine(m_myTransform.position, m_myTransform.position + m_myTransform.forward * 5f);
+        }
+    }
+
+    private void AngleCalculate()
+    {
+        m_rotY += Input.GetAxis("Mouse X");
+        m_rotX = Mathf.Clamp(m_rotX += -Input.GetAxis("Mouse Y"), m_limitDownVerticalAngle, m_limitUpVerticalAngle);
+
+        m_myTransform.eulerAngles = new Vector3(m_rotX, m_rotY, 0.0f);
+        m_myTransform.position = m_ownerTransform.position + m_myTransform.rotation * m_distanceOffset;
+
+        float angleA = m_myTransform.rotation.eulerAngles.y > 180 ? m_myTransform.rotation.eulerAngles.y - 360f : m_myTransform.rotation.eulerAngles.y;
+        float angleB = m_ownerTransform.rotation.eulerAngles.y > 180 ? m_ownerTransform.rotation.eulerAngles.y - 360f : m_ownerTransform.rotation.eulerAngles.y;
+        float _signedAngle = angleA - angleB;
+
+        //Vector3 forwardA = transform.rotation * Vector3.forward;
+        //Vector3 forwardB = target.rotation * Vector3.forward;
+
+        //float angleA = Mathf.Atan2(forwardA.x, forwardA.z) * Mathf.Rad2Deg;
+        //float angleB = Mathf.Atan2(forwardB.x, forwardB.z) * Mathf.Rad2Deg;
+
+        //float _signedAngle = Mathf.DeltaAngle(angleA, angleB);
+
+        m_ownerAnim.SetFloat("horAngle", _signedAngle);
+        m_ownerAnim.SetFloat("verAngle", m_rotX);
+    }
+}
