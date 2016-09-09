dd303e90533ecc998d81d5575c655c5bc0eb7c00
Deageun Park <muramasa666@naver.com>
2016-09-10 03:20:04 +0900

    Entry 프리팹 수정, Task 스크립트 추가

---
+++ Assets/Scripts/TaskCtrl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public enum TaskSpriteType { NORMAL = 0, SUCCESS }
+
+public class TaskCtrl : MonoBehaviour {
+    [SerializeField] private Sprite m_normalSprite;
+    [SerializeField] private Sprite m_successSprtie;
+    [SerializeField] private TaskSpriteType m_currentSpriteType;
+
+    private GameObject m_myGameObject;
+    private Image m_myImage;
+    private Text  m_myText;
+
+    public TaskSpriteType TaskSpriteType
+    {
+        get { return m_currentSpriteType; }
+        set
+        {
+            m_currentSpriteType = value;
+            m_myImage.sprite = (m_currentSpriteType == TaskSpriteType.NORMAL) ? m_normalSprite : m_successSprtie;
+        }
+    }
+
+    public string Message
+    {
+        get { return m_myText.text; }
+        set { m_myText.text = value; }
+    }
+
+    public bool GameObjectActive
+    {
+        get { return m_myGameObject.activeSelf; }
+        set { m_myGameObject.SetActive(value); }
+    }
+
+	void Awake () {
+        m_myGameObject = gameObject;
+        m_myImage      = GetComponent<Image>();
+        m_myText       = GetComponentInChildren<Text>();
+        TaskSpriteType = m_currentSpriteType;
+	}
+}
---
+++ Assets/Scripts/TaskManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaskManager : MonoBehaviour {
+    [SerializeField] private TaskCtrl[] m_taskCtrlArray;
+
+	void Start () {
+	}
+	
+	void Update () {
+	}
+}
