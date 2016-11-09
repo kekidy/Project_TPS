58093d046a2d648acdb84dae27f03a9b8bd6f9ab
unknown <>
2016-11-09 15:02:41 +0900

    라이트 수정

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+    public GameObject testobj;
+
+	// Use this for initialization
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(3f);
+
+        var oper = SceneManager.LoadSceneAsync("TestScene", LoadSceneMode.Additive);
+        while (!oper.isDone)
+            yield return null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
