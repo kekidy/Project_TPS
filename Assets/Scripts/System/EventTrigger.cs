63e897946b51fd22edcd379c44119cc7ad8628e4
Daegeun Park <muramasa666@naver.com>
2016-12-04 23:56:52 +0900

    맵 동적 액티브 완성.

--- Assets/Scripts/System/EventTrigger.cs
+++ Assets/Scripts/System/EventTrigger.cs
@@ -4,14 +4,17 @@ using System.Collections;
 
 [RequireComponent(typeof(BoxCollider))]
 public class EventTrigger : MonoBehaviour {
-    [SerializeField] private GameObject m_eventTarget = null;
+    [SerializeField] private GameObject m_eventTarget        = null;
     [SerializeField] private float      m_startDelaySeconds  = 0f;
-    [SerializeField] private UnityEvent m_onEvent     = null;
+    [SerializeField] private UnityEvent m_onEvent            = null;
+    [SerializeField] private bool       m_autoDestroy        = true;
 
     void OnEvent()
     {
         m_onEvent.Invoke();
-        Destroy(gameObject);
+
+        if (m_autoDestroy)
+            Destroy(gameObject);
     }
 
     void OnTriggerEnter(Collider col)
