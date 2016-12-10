368e223894f57d84757e1c709e0c95042e3c3475
Deageun Park <muramasa666@naver.com>
2016-12-10 18:06:39 +0900

    가이드 텍스트 배치 중

---
+++ Assets/Scripts/System/TextHideCtrl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextHideCtrl : MonoBehaviour {
+    [SerializeField] private Transform m_targetTransform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
